b8a70a1fa69a0ff07243e475215f78e255358c6f
nada-mah <cutenada.lara4@gmail.com>
2026-04-19 23:27:31 +0200

    modified:   app/config.py

--- app/config.py
+++ app/config.py
@@ -3,9 +3,9 @@ LLM_REPO_ID = "unsloth/Qwen3-0.6B-GGUF"
 LLM_FILENAME = "Qwen3-0.6B-BF16.gguf"
 
 # For production
-# LLM_REPO_ID_GPU = "bartowski/Qwen2.5-14B-Instruct-GGUF"
-# LLM_FILENAME_GPU = "Qwen2.5-14B-Instruct-Q4_K_M.gguf"
+LLM_REPO_ID_GPU = "bartowski/Qwen2.5-14B-Instruct-GGUF"
+LLM_FILENAME_GPU = "Qwen2.5-14B-Instruct-Q4_K_M.gguf"
 # LLM_REPO_ID_GPU = "MaziyarPanahi/Mistral-Nemo-Instruct-2407-GGUF"
 # LLM_FILENAME_GPU = "Mistral-Nemo-Instruct-2407.Q4_K_M.gguf"
-LLM_REPO_ID_GPU = "unsloth/Qwen3-14B-GGUF"
-LLM_FILENAME_GPU = "Qwen3-14B-Q4_K_M.gguf"
+# LLM_REPO_ID_GPU = "unsloth/Qwen3-14B-GGUF"
+# LLM_FILENAME_GPU = "Qwen3-14B-Q4_K_M.gguf"
